ae77dbb8f2cf3c7e5424d9f06cfb1bfbddd169c7
IanKhov <167153877+IanKhov@users.noreply.github.com>
2026-02-06 12:43:08 +0000

    part1 c finished

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,39 +1,35 @@
-const Hello = (props) => {
-	console.log(props)
-	return (
-		<div>
-			<p>Hello {props.name}, you are {props.age} years old</p>
-		</div>
-	)
-}
+import { useState } from "react"
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-	const now = new Date()
-	const a = 10
-	const b = 20
-	const name = "Cam"
-	console.log(now, a+b)
+  const [ counter, setCounter ] = useState(0)
 
-	const friends = [
-		{name: 'Bob', age: 67},
-		{name: 'Ward', age: 21}
-	]
-	
-	return (
-		<>
-			<h1>Greetings</h1>
-			<p>it is {now.toString()}</p>
-			<Hello name="George" age="?"/>
-			<Hello name="Ian" age={26 + 15}/>
-			<Hello name={name} age={a + b}/>
-			<p>
-				{a} plus {b} is equal to {a + b}
-			</p>
+  const increaseByOne = () => setCounter(counter + 1)
 
-			<p>{friends[0].name} {friends[0].age}</p>
-			<p>{friends[1].name} {friends[1].age}</p>
-		</>
-	)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  return (
+    <div>
+      <Display counter={counter}/>
+
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />           
+    </div>
+  )
 }
 
 export default App
