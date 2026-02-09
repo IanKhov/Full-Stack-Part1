05bf51500ad37539c709fd53215d143ae1070807
IanKhov <167153877+IanKhov@users.noreply.github.com>
2026-02-09 11:17:41 +0000

    Starting part2

--- part1_exercises/src/main.jsx
+++ part1_exercises/src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
 
-import App from './App'
+import App from '../exercise1.12-1.14/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
